2757ce6ee9c44fd408dad9871700fb2c759da4c1
Kleber Petry <kleberpetry@gmail.com>
2022-04-05 20:01:39 -0300

    Create 1071 - soma de impares consecutivos 1.c

---
+++ 1071 - soma de impares consecutivos 1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main(){
+	int x,y,soma=0,j;
+	scanf("%d %d",&x,&y);
+	if(x>y){
+		for(x=x-1;x>y;x--){
+			if((x%2)!=0){
+				soma+=x;
+			}
+		}
+	}else{
+		for(x=x+1;x<y;x++){
+			if((x%2)!=0){
+				soma+=x;
+			}
+		}
+	}
+	printf("%d",soma);
+		
+	
+	return 0;
+}
