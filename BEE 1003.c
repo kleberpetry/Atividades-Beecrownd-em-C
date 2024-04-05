9d3f22cc3d5b0c1d99cd08742413601f8a06ce8e
Kleber Petry <kleberpetry@gmail.com>
2024-04-05 08:45:07 -0300

    05-04

---
+++ BEE 1003.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+ 	int num1,num2,resultado;
+ 	scanf("%d %d",&num1,&num2);
+ 	resultado=num1+num2;
+ 	printf("SOMA = %d\n",resultado);
+   
+ 
+    return 0;
+}
---
+++ BEE 1004.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main() {
+ 	int v1,v2,PROD;
+	scanf("%d %d",&v1,&v2);
+	PROD=v1*v2;
+	printf("PROD = %d\n",PROD);
+ 
+    return 0;
+}
