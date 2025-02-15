2289ba1b7d1782f34cb7c6a151ba098f7898a28a
Kleber Petry <kleberpetry@gmail.com>
2025-02-15 19:34:20 -0300

    1010 youtube

---
+++ 1010.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	int cod1, cod2, qtd1, qtd2;
+	double valor1, valor2, total;
+	
+	//leitura primeira linha
+	scanf("%d %d %lf", &cod1, &qtd1, &valor1);
+	//leitura segunda linha
+	scanf("%d %d %lf", &cod2, &qtd2, &valor2);
+	
+	//calculo total
+	total= (qtd1*valor1)+(qtd2*valor2);
+	
+	printf("VALOR A PAGAR: R$ %.2lf\n",total);
+	
+	
+	return 0;
+}
